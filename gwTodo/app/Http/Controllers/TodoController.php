2e4b7e4d8b2a3603a0c9329f8980112949b39664
YuseiNakahara <39870505+YuseiNakahara@users.noreply.github.com>
2019-05-23 12:34:02 +0900

    show削除
    使わないので消しましょう。

--- gwTodo/app/Http/Controllers/TodoController.php
+++ gwTodo/app/Http/Controllers/TodoController.php
@@ -48,16 +48,7 @@ class TodoController extends Controller
         return redirect()->to('todo');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+   
 
     /**
      * Show the form for editing the specified resource.
